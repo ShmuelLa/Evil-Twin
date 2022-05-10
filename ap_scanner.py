28024d9ae1ebe1d919bd55b1bd99f029f3f789dc
kolron <kolskyron@gmail.com>
2022-05-10 10:23:23 +0000

    Fixed ap_scanner and added defence_v0

--- ap_scanner.py
+++ ap_scanner.py
@@ -2,7 +2,7 @@ from scapy.all import *
 import multiprocessing
 import time
 import os
-from scapy.layers.dot11 import Dot11Beacon, Dot11Elt, Dot11, Dot11ProbeResp
+from scapy.layers.dot11 import Dot11Beacon, Dot11Elt, Dot11
 
 """
 Use:
@@ -10,6 +10,11 @@ Run main (or wrap in another function), enter interface to fit your card.
 wait until scan if complete and choose a network
 returns a tuple of (mac address,ap name,channel, and signal dbm)
 """
+
+global network_dict
+network_dict = {}  # holds network stats - mac,name,channel
+network_index = {}  # for easy access to network using index
+my_macs = [] #= [get_if_hwaddr(i) for i in get_if_list()] # so we don't attack ourselves
 
 def sniffAP(packet):
     if packet.haslayer(Dot11Beacon):
@@ -26,8 +31,9 @@ def sniffAP(packet):
         stats = packet[Dot11Beacon].network_stats()
         # get the channel of the AP
         channel = stats.get("channel")
-        if mac_addr not in network_dict.keys():
+        if mac_addr not in network_dict.keys()and mac_addr not in my_macs:
             network_dict[mac_addr] = (mac_addr,ap_name, channel, dbm_signal)
+            print(f"Found AP:{network_dict[mac_addr]}")
 
 
 # changing channel every 2 seconds, for timeout times.
@@ -49,8 +55,8 @@ def changeChannel(timeout: int):
 def pickNetwork():
     mac_ok = False
     while not mac_ok:
-        network_mac = input("Please enter a valid index or MAC address of the network you want to attack: ")
-        if len(network_mac) < 16:  # shorter than mac address TODO: this can be problematic in case we have a lot of networks scanned.
+        network_mac = input("Please enter an index (starting from 0) or MAC address of the network you want to attack: ")
+        if not  any(c.isalpha() for c in str(network_mac)): #if index is entered there will be no letters
             if int(network_mac) not in network_index.keys():
                 print("ERROR, not a valid index")
                 continue
@@ -68,24 +74,27 @@ def pickNetwork():
     return network_dict.get(network_mac)
 
 
-if __name__ == "__main__":
-    global network_dict
-    network_dict = {}  # holds network stats - mac,name,channel
-    network_index = {}  # for easy access to network using index
+def setMonitor(interface):
+    os.system(f"sudo ifconfig {interface} down")
+    os.system(f"sudo iwconfig {interface} mode monitor")
+    os.system(f"sudo ifconfig {interface} up")
 
+
+if __name__ == "__main__":
+    # interface name
+    interface = "wlx5ca6e686a840"
+    setMonitor(interface)
     # if no timeout is passed, default to 60 seconds
     if len(sys.argv) > 1:
         timeout = int(sys.argv[1])
     else:
         timeout = 60
 
-    # interface name
-    interface = "wlx5ca6e686a840"
     # start the thread that changes channels all the networks
-    channel_changer = multiprocessing.Process(target=changeChannel(timeout))
+    channel_changer = multiprocessing.Process(target=changeChannel,args=(timeout,),daemon=True)
     channel_changer.start()
-    sniff(prn=sniffAP, iface=interface, timeout=timeout)
-
+    sniff(prn=sniffAP,iface=interface, timeout=timeout)
+    channel_changer.join()
     # Create network index dict, and prompt picking of a network.
     i = 0
     print("Available networks:")
