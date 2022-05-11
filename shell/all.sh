4b6b84adc0c09d509966c3b36d1dbb687cfe5670
kolron <kolskyron@gmail.com>
2022-05-11 01:32:37 +0000

    portal

--- shell/all.sh
+++ shell/all.sh
@@ -2,20 +2,20 @@
 # sudo systemctl stop NetworkManager
 # sudo airmon-ng check kill
 
-# sudo ifconfig wlan1 down
-# sudo iwconfig wlan1 mode monitor
-# sudo ifconfig wlan1 up
+# sudo ifconfig wlx5ca6e686a840 down
+# sudo iwconfig wlx5ca6e686a840 mode monitor
+# sudo ifconfig wlx5ca6e686a840 up
 
-sudo ip link set wlan1 down
-sudo iw dev wlan1 set type monitor
-sudo ip link set wlan1 up
+sudo ifconfig wlx5ca6e686a840 down
+sudo iwconfig wlx5ca6e686a840 mode monitor
+sudo ifconfig wlx5ca6e686a840 up
 
-sudo ifconfig wlan1 up 10.100.101.1 netmask 255.255.255.0
+sudo ifconfig wlx5ca6e686a840 up 10.100.101.1 netmask 255.255.255.0
 sudo route add -net 10.100.101.0 netmask 255.255.255.0 gw 10.100.101.1
-sudo ip link set wlan1 up
+sudo ip link set wlx5ca6e686a840 up
 
-sudo iptables --table nat --append POSTROUTING --out-interface wlan0 -j MASQUERADE
-sudo iptables --append FORWARD --in-interface wlan1 -j ACCEPT
+sudo iptables --table nat --append POSTROUTING --out-interface wlo1 -j MASQUERADE
+sudo iptables --append FORWARD --in-interface wlx5ca6e686a840 -j ACCEPT
 sudo echo 1 > /proc/sys/net/ipv4/ip_forward
 sudo hostapd config/hostapd.conf
 
