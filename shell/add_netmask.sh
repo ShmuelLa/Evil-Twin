f008448e11174ac7c3d468b0adf46506840dddf4
ShmuelLa <shmuel516@gmil.com>
2022-05-12 00:35:51 +0000

    added captive portal v2

--- shell/add_netmask.sh
+++ shell/add_netmask.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo ifconfig wlx5ca6e686a840 up 10.100.101.1 netmask 255.255.255.0
+sudo ifconfig wlan1 up 10.100.101.1 netmask 255.255.255.0
 sudo route add -net 10.100.101.0 netmask 255.255.255.0 gw 10.100.101.1
-sudo ip link set wlx5ca6e686a840 up
+sudo ip link set wlan1 up
