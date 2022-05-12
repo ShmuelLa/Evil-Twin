9f9366d9dd3401bcafe2b02779332d6d7b97972a
kolron <kolskyron@gmail.com>
2022-05-12 16:07:36 +0000

    change ap scanner

--- ap_scanner.py
+++ ap_scanner.py
@@ -14,30 +14,29 @@ returns a tuple of (mac address,ap name,channel, and signal dbm)
 global network_dict
 network_dict = {}  # holds network stats - mac,name,channel
 network_index = {}  # for easy access to network using index
-my_macs = [] #= [get_if_hwaddr(i) for i in get_if_list()] # so we don't attack ourselves
-client_dict = {}
-client_index = {}
+my_macs = []  # = [get_if_hwaddr(i) for i in get_if_list()] # so we don't attack ourselves
+ap_client_list = {}
 
 
-def sniffAP(packet):
-    if packet.haslayer(Dot11Beacon):
+def sniffAP(pkt):
+    if pkt.haslayer(Dot11Beacon):
         # extract the MAC address of the network
-        mac_addr = packet[Dot11].addr2
+        mac_addr = pkt[Dot11].addr2
         # get the name of it
-        ap_name = packet[Dot11Elt].info.decode()
+        ap_name = pkt[Dot11Elt].info.decode()
         # get dbm signal
         try:
-            dbm_signal = packet.dBm_AntSignal
+            dbm_signal = pkt.dBm_AntSignal
         except:
             dbm_signal = "N/A"
         # extract network stats for channel
-        stats = packet[Dot11Beacon].network_stats()
+        stats = pkt[Dot11Beacon].network_stats()
         # get the channel of the AP
         channel = stats.get("channel")
-        if mac_addr not in network_dict.keys()and mac_addr not in my_macs:
+        if mac_addr not in network_dict.keys() and mac_addr not in my_macs:
             network_dict[mac_addr] = (mac_addr, ap_name, channel, dbm_signal)
             print(f"Found AP:{network_dict[mac_addr]}")
-
+            ap_client_list[mac_addr] = []
 
 # changing channel every 2 seconds, for timeout times.
 def changeChannel(timeout: int, interface):
@@ -58,8 +57,9 @@ def changeChannel(timeout: int, interface):
 def pickNetwork():
     mac_ok = False
     while not mac_ok:
-        network_mac = input("Please enter an index (starting from 0) or MAC address of the network you want to attack: ")
-        if not any(c.isalpha() for c in str(network_mac)): #if index is entered there will be no letters
+        network_mac = input(
+            "Please enter an index (starting from 0) or MAC address of the network you want to attack: ")
+        if not any(c.isalpha() for c in str(network_mac)):  # if index is entered there will be no letters
             if int(network_mac) not in network_index.keys():
                 print("ERROR, not a valid index")
                 continue
@@ -76,37 +76,47 @@ def pickNetwork():
             return network_dict.get(network_mac)
 
 
-def scanClients(target_ap):
-    def packet_handler(packet):
-        if (packet.addr2 == target_ap or packet.addr3 == target_ap) and packet.addr1 != 'ff:ff:ff:ff':
-            if packet.addr1 not in client_dict[target_ap].keys() and packet.addr2!=packet.addr1 and packet.addr1!=packet.addr3:
-                client_dict[target_ap][packet.addr1] = (packet.addr1, packet.addr2)
-                print(f"Found a possible client: MAC: {client_dict[target_ap][packet.addr1][0]} | Name: {client_dict[target_ap][packet.addr1][1]}")
+def scanClients(packet):
+    if packet.FCfield:
+        DS = packet.FCfield & 0x3
+        to_DS = DS & 0x1 != 0
+        from_DS = DS & 0x2 != 0
+
+        #  addr1 is ap | addr2 is client,
+        if to_DS and not from_DS:
+            client_mac = packet.addr2
+            ap_mac = packet.addr1
+            print(f"AP: {ap_mac} CLIENT: {client_mac}")
+            if packet.addr2 not in ap_client_list.setdefault(packet.addr1, []):
+                print(f"Possible client: {client_mac}")
+                ap_client_list[packet.addr1].append(packet.addr2)
+
+# if (packet.addr2 == target_ap or packet.addr3 == target_ap) and packet.addr1 != "ff:ff:ff:ff:ff:ff":
+#       if packet.addr1 not in client_list:
+#          if packet.addr2 != packet.addr1 and packet.addr1 != packet.addr3:
+#             client_list.append(packet.addr1)
 
 
-def pickClient(target_ap):
+def pickClient():
     client_ok = False
     while not client_ok:
         client_mac = input("Please enter an index (starting from 0) or MAC address of the client you want to attack: ")
-        if not any(c.isalpha() for c in str(client_mac)): #if index is entered there will be no letters
-            if int(client_mac) not in client_dict[target_ap].keys():
-                print("ERROR, not a valid index")
-                continue
-            else:
-                print("Valid index")
-                #equivelent to client_dict[target_ap].get(client_index[target_ap].get(int(client_mac)))[0]
-                client_mac = client_index[target_ap].get(int(client_mac))
-                print(f"MAC chosen: {client_mac}")
-                return client_mac
-        if client_mac not in client_dict[target_ap].keys():
+        # if not any(c.isalpha() for c in str(client_mac)):  # if index is entered there will be no letters
+        #     if int(client_mac) > len(client_list):
+        #         print("ERROR, not a valid index")
+        #         continue
+        #     else:
+        #         print("Valid index")
+        #         client_mac = client_list[int(client_mac)]
+        #         print(f"MAC chosen: {client_mac}")
+        #         return client_mac
+        if client_mac not in ap_client_list[target_ap]:
             print("Error, not a valid MAC address")
             continue
-        if client_mac in client_dict[target_ap].keys():
-            #in ap client dict, get the mac address stored in tuple[0]
-            client_mac = client_dict[target_ap].get(client_mac)[0]
+        if client_mac in ap_client_list[target_ap]:
+            # in ap client dict, get the mac address stored in tuple[0]
             print(f"MAC chosen: {client_mac}")
             return client_mac
-    return client_mac
 
 
 def setMonitor(interface):
@@ -116,10 +126,8 @@ def setMonitor(interface):
 
 
 def ap_client_scanner(interface):
-    """
-    return: ((mac_addr, ap_name, channel, dbm_signal), chosen_client_mac)
-    """
     # interface name
+    global target_ap
     setMonitor(interface)
     # if no timeout is passed, default to 60 seconds
     timeout = 30
@@ -127,9 +135,9 @@ def ap_client_scanner(interface):
     # ----------------------------PART 1: scan and pick network ---------------------
     # start the thread that changes channels all the networks
 
-    channel_changer = multiprocessing.Process(target=changeChannel,args=(timeout,interface),daemon=True)
+    channel_changer = multiprocessing.Process(target=changeChannel, args=(timeout, interface), daemon=True)
     channel_changer.start()
-    sniff(prn=sniffAP,iface=interface, timeout=timeout)
+    sniff(prn=sniffAP, iface=interface, timeout=timeout)
     channel_changer.join()
     # Create network index dict, and prompt picking of a network.
     i = 0
@@ -139,7 +147,7 @@ def ap_client_scanner(interface):
         network_name = network_dict.get(network)[1]
         channel = network_dict.get(network)[2]
         dbm = network_dict.get(network)[3]
-        print(f"Index: {i} | MAC: {network_addr} | NAME: {network_name} | CHANNEL: {channel} | SIGNAL DBM: {dbm}")
+        print(f"INDEX: {i} | MAC: {network_addr} | NAME: {network_name} | CHANNEL: {channel} | SIGNAL DBM: {dbm}")
         network_index[i] = network
         i += 1
     chosen_network = pickNetwork()
@@ -147,19 +155,18 @@ def ap_client_scanner(interface):
     # --------------------------------------------------------------------------------------------------
     # ----------------------------PART 2: scan and pick client from chosen network ---------------------
 
-    client_dict[chosen_network_mac] = {}  # define a dictionary for each ap inside a general dict
-    channel_changer = multiprocessing.Process(target=changeChannel, args=(timeout,interface), daemon=True)
+    # define a dictionary for each ap inside a general dict
+    channel_changer = multiprocessing.Process(target=changeChannel, args=(timeout, interface), daemon=True)
     channel_changer.start()
-    sniff(iface = interface, prn=scanClients(chosen_network_mac), timeout=timeout)
+    target_ap = chosen_network_mac
+    sniff(iface=interface, prn=scanClients, timeout=timeout)
     channel_changer.join()
     i = 0
     print("Possible Clients:")
-    for client in client_dict[chosen_network_mac].keys():
-        client_mac = client_dict[chosen_network_mac][client][0]
-        print(f"MAC:{client_mac}")
-        client_index[chosen_network_mac][i] = client_dict[chosen_network_mac][0]
+    for client in ap_client_list[target_ap]:
+        client_mac = client
+        print(f"Index: {i} | MAC:{client_mac}")
         i += 1
-    chosen_client_mac = pickClient(chosen_network_mac)
-    return chosen_network, chosen_client_mac
+    chosen_client_mac = pickClient()
+    print(chosen_client_mac)
 
-    
